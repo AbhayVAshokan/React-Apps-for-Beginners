9e2167152afd96d0f0ff07c097fb80e93ced5e7d
AbhayVAshokan <abhayvashokan@gmail.com>
2021-01-03 00:14:23 +0530

    Migrated Calculator to TypeScript

--- src/components/Calculator/Calculator.tsx
+++ src/components/Calculator/Calculator.tsx
@@ -3,30 +3,31 @@ import { parse, eval as evaluate } from 'expression-eval';
 
 import "./Calculator.css";
 
-const Calculator = () => {
-    let [result, setResult] = useState(0);
-    let [history, setHistory] = useState("");
-    let [clearMode, setClearMode] = useState(false);
-    let [finalResult, setFinalResult] = useState(0);
+const Calculator: React.FunctionComponent<{}> = () => {
+    let [result, setResult] = useState<string>("0");
+    let [history, setHistory] = useState<string>("");
+    let [clearMode, setClearMode] = useState<boolean>(false);
+    let [finalResult, setFinalResult] = useState<string>("0");
+
 
     // Function to calculate the result of the entered expression
-    const calculate = () => {
+    const calculate: () => string = () => {
         let expression = history.replaceAll("x", "*");
         expression = expression.replaceAll(" ", "");
 
-        if("+/-%*".includes(expression[expression.length - 1]))
+        if ("+/-%*".includes(expression[expression.length - 1]))
             expression += result;
 
-        console.log(expression);
-        return evaluate(parse(expression));
+        return evaluate(parse(expression), {});
     }
+
 
     // Function triggered each time a digit is pressed
     // Append the digit to the number displayed
-    const onDigitClick = (num) => {
-        if (result !== 0 && result.length === 9)
+    const onDigitClick: (num: string) => void = (num) => {
+        if (result !== "0" && result.length === 9)
             return;
-        if (result === 0 || clearMode)
+        if (result === "0" || clearMode)
             result = num;
         else
             result += num;
@@ -36,34 +37,36 @@ const Calculator = () => {
             setClearMode(false);
     }
 
+
     // Function triggered each time an operator is pressed
     // Calculate the intermediate result | Update history
-    const onOperatorClick = (op) => {
+    const onOperatorClick: (op: string) => void = (op) => {
         console.log(finalResult);
-        if (result === 0 || clearMode)
+        if (result === "0" || clearMode)
             return;
 
         history += `${result} ${op} `;
         setHistory(history);
         setClearMode(true);
-        
     }
-    
+
+
     // Function triggered when equal to button is pressed
     // Final result evaluated
-    const onEquals = () => {
+    const onEquals: () => void = () => {
         result = calculate();
         setResult(result);
         setHistory("");
         setClearMode(true);
     }
 
+
     // Function triggered when clear button is pressed
     // Reset to defaults
-    const onClear = () => {
-        setResult(0);
+    const onClear: () => void = () => {
+        setResult("0");
         setHistory("");
-        setFinalResult(0);
+        setFinalResult("0");
         setClearMode(false);
     }
 
